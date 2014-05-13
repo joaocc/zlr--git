ff10c047f328727319f6e2cb683f29b1742feb15
Jesse McGrew <jmcgrew@hansprestige.com>
2014-05-13 09:13:12 -0700

    Factored common TestCaseIO methods out into the base class.

--- TestSuite/TestCaseIO.cs
+++ TestSuite/TestCaseIO.cs
@@ -6,8 +6,9 @@ using System.IO;
 
 namespace TestSuite
 {
-    abstract class TestCaseIO
+    abstract class TestCaseIO : IZMachineIO
     {
+        protected readonly Queue<string> inputBuffer = new Queue<string>(); 
         protected readonly StringBuilder outputBuffer = new StringBuilder();
 
         public string CollectOutput()
@@ -16,11 +17,233 @@ namespace TestSuite
             outputBuffer.Length = 0;
             return result;
         }
+
+        #region IZMachineIO Members
+
+        public abstract string ReadLine(string initial, int time, TimedInputCallback callback, byte[] terminatingKeys, out byte terminator);
+
+        public abstract short ReadKey(int time, TimedInputCallback callback, CharTranslator translator);
+
+        public void PutCommand(string command)
+        {
+            // nada
+        }
+
+        public abstract void PutChar(char ch);
+
+        public abstract void PutString(string str);
+
+        public abstract void PutTextRectangle(string[] lines);
+
+        public bool Buffering
+        {
+            get { return false; }
+            set { /* nada */ }
+        }
+
+        public bool Transcripting
+        {
+            get { return false; }
+            set { /* nada */ }
+        }
+
+        public void PutTranscriptChar(char ch)
+        {
+            // nada
+        }
+
+        public void PutTranscriptString(string str)
+        {
+            // nada
+        }
+
+        public Stream OpenSaveFile(int size)
+        {
+            return null;
+        }
+
+        public Stream OpenRestoreFile()
+        {
+            return null;
+        }
+
+        public Stream OpenAuxiliaryFile(string name, int size, bool writing)
+        {
+            return null;
+        }
+
+        public abstract Stream OpenCommandFile(bool writing);
+
+        public void SetTextStyle(TextStyle style)
+        {
+            // nada
+        }
+
+        public void SplitWindow(short lines)
+        {
+            // nada
+        }
+
+        public void SelectWindow(short num)
+        {
+            // nada
+        }
+
+        public void EraseWindow(short num)
+        {
+            // nada
+        }
+
+        public void EraseLine()
+        {
+            // nada
+        }
+
+        public void MoveCursor(short x, short y)
+        {
+            // nada
+        }
+
+        public void GetCursorPos(out short x, out short y)
+        {
+            x = 1;
+            y = 1;
+        }
+
+        public void SetColors(short fg, short bg)
+        {
+            // nada
+        }
+
+        public short SetFont(short num)
+        {
+            // not supported
+            return 0;
+        }
+
+        public bool DrawCustomStatusLine(string location, short hoursOrScore, short minsOrTurns, bool useTime)
+        {
+            return false;
+        }
+
+        public void PlaySoundSample(ushort number, SoundAction action, byte volume, byte repeats, SoundFinishedCallback callback)
+        {
+            // nada
+        }
+
+        public virtual void PlayBeep(bool highPitch)
+        {
+            // nada
+        }
+
+        public bool ForceFixedPitch
+        {
+            get { return true; }
+            set { /* nada */ }
+        }
+
+        public bool VariablePitchAvailable
+        {
+            get { return false; }
+        }
+
+        public bool ScrollFromBottom
+        {
+            get { return false; }
+            set { /* nada */ }
+        }
+
+        public bool BoldAvailable
+        {
+            get { return false; }
+        }
+
+        public bool ItalicAvailable
+        {
+            get { return false; }
+        }
+
+        public bool FixedPitchAvailable
+        {
+            get { return false; }
+        }
+
+        public bool GraphicsFontAvailable
+        {
+            get { return false; }
+        }
+
+        public bool TimedInputAvailable
+        {
+            get { return false; }
+        }
+
+        public bool SoundSamplesAvailable
+        {
+            get { return false; }
+        }
+
+        public byte WidthChars
+        {
+            get { return 80; }
+        }
+
+        public short WidthUnits
+        {
+            get { return 80; }
+        }
+
+        public byte HeightChars
+        {
+            get { return 25; }
+        }
+
+        public short HeightUnits
+        {
+            get { return 25; }
+        }
+
+        public byte FontHeight
+        {
+            get { return 1; }
+        }
+
+        public byte FontWidth
+        {
+            get { return 1; }
+        }
+
+        public event EventHandler SizeChanged
+        {
+            add { /* nada */ }
+            remove { /* nada */ }
+        }
+
+        public bool ColorsAvailable
+        {
+            get { return false; }
+        }
+
+        public byte DefaultForeground
+        {
+            get { return 9; }
+        }
+
+        public byte DefaultBackground
+        {
+            get { return 2; }
+        }
+
+        public UnicodeCaps CheckUnicode(char ch)
+        {
+            return UnicodeCaps.CanPrint | UnicodeCaps.CanInput;
+        }
+
+        #endregion
     }
 
-    class ReplayIO : TestCaseIO, IZMachineIO
+    class ReplayIO : TestCaseIO
     {
-        private readonly Queue<string> inputBuffer = new Queue<string>();
         private readonly string inputFile;
 
         public ReplayIO(string prevInputFile)
@@ -28,258 +251,45 @@ namespace TestSuite
             this.inputFile = prevInputFile;
         }
 
-        #region Z-machine I/O implementation
-
-        string IZMachineIO.ReadLine(string initial, int time, TimedInputCallback callback,
-            byte[] terminatingKeys, out byte terminator)
+        public override string ReadLine(string initial, int time, TimedInputCallback callback, byte[] terminatingKeys, out byte terminator)
         {
             terminator = 13;
             return inputBuffer.Dequeue();
         }
 
-        void IZMachineIO.PutCommand(string command)
-        {
-            // nada
-        }
-
-        short IZMachineIO.ReadKey(int time, TimedInputCallback callback, CharTranslator translator)
+        public override short ReadKey(int time, TimedInputCallback callback, CharTranslator translator)
         {
             string inputLine;
             do { inputLine = inputBuffer.Dequeue(); } while (inputLine.Length == 0);
             return translator(inputLine[0]);
         }
 
-        void IZMachineIO.PutChar(char ch)
+        public override void PutChar(char ch)
         {
             outputBuffer.Append(ch);
         }
 
-        void IZMachineIO.PutString(string str)
+        public override void PutString(string str)
         {
             outputBuffer.Append(str);
         }
 
-        void IZMachineIO.PutTextRectangle(string[] lines)
+        public override void PutTextRectangle(string[] lines)
         {
             foreach (string line in lines)
                 outputBuffer.AppendLine(line);
         }
 
-        bool IZMachineIO.Transcripting
-        {
-            get { return false; }
-            set { /* nada */}
-        }
-
-        void IZMachineIO.PutTranscriptChar(char ch)
-        {
-            // nada
-        }
-
-        void IZMachineIO.PutTranscriptString(string str)
-        {
-            // nada
-        }
-
-        Stream IZMachineIO.OpenSaveFile(int size)
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenRestoreFile()
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenAuxiliaryFile(string name, int size, bool writing)
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenCommandFile(bool writing)
+        public override Stream OpenCommandFile(bool writing)
         {
             if (writing)
                 return null;
 
             return new FileStream(inputFile, FileMode.Open, FileAccess.Read);
         }
-
-        void IZMachineIO.SetTextStyle(TextStyle style)
-        {
-            // nada
-        }
-
-        void IZMachineIO.SplitWindow(short lines)
-        {
-            // nada
-        }
-
-        void IZMachineIO.SelectWindow(short num)
-        {
-            // nada
-        }
-
-        void IZMachineIO.EraseWindow(short num)
-        {
-            // nada
-        }
-
-        void IZMachineIO.EraseLine()
-        {
-            // nada
-        }
-
-        void IZMachineIO.MoveCursor(short x, short y)
-        {
-            // nada
-        }
-
-        void IZMachineIO.GetCursorPos(out short x, out short y)
-        {
-            x = 1;
-            y = 1;
-        }
-
-        void IZMachineIO.SetColors(short fg, short bg)
-        {
-            // nada
-        }
-
-        short IZMachineIO.SetFont(short num)
-        {
-            // not supported
-            return 0;
-        }
-
-        bool IZMachineIO.GraphicsFontAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.ForceFixedPitch
-        {
-            get { return true; }
-            set { /* nada */ }
-        }
-
-        bool IZMachineIO.BoldAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.ItalicAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.FixedPitchAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.VariablePitchAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.ScrollFromBottom
-        {
-            get { return false; }
-            set { /* nada */ }
-        }
-
-        bool IZMachineIO.TimedInputAvailable
-        {
-            get { return false; }
-        }
-
-        byte IZMachineIO.WidthChars
-        {
-            get { return 80; }
-        }
-
-        short IZMachineIO.WidthUnits
-        {
-            get { return 80; }
-        }
-
-        byte IZMachineIO.HeightChars
-        {
-            get { return 25; }
-        }
-
-        short IZMachineIO.HeightUnits
-        {
-            get { return 25; }
-        }
-
-        byte IZMachineIO.FontHeight
-        {
-            get { return 1; }
-        }
-
-        byte IZMachineIO.FontWidth
-        {
-            get { return 1; }
-        }
-
-        event EventHandler IZMachineIO.SizeChanged
-        {
-            add { /* nada */ }
-            remove { /* nada */ }
-        }
-
-        bool IZMachineIO.ColorsAvailable
-        {
-            get { return false; }
-        }
-
-        byte IZMachineIO.DefaultForeground
-        {
-            get { return 9; }
-        }
-
-        byte IZMachineIO.DefaultBackground
-        {
-            get { return 2; }
-        }
-
-        void IZMachineIO.PlaySoundSample(ushort num, SoundAction action, byte volume, byte repeats,
-            SoundFinishedCallback callback)
-        {
-            // nada
-        }
-
-        void IZMachineIO.PlayBeep(bool highPitch)
-        {
-            // nada
-        }
-
-        bool IZMachineIO.SoundSamplesAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.Buffering
-        {
-            get { return false; }
-            set { /* nada */ }
-        }
-
-        UnicodeCaps IZMachineIO.CheckUnicode(char ch)
-        {
-            return UnicodeCaps.CanPrint | UnicodeCaps.CanInput;
-        }
-
-        bool IZMachineIO.DrawCustomStatusLine(string location, short hoursOrScore, short minsOrTurns, bool useTime)
-        {
-            return false;
-        }
-
-        #endregion
     }
 
-    class RecordingIO : TestCaseIO, IZMachineIO
+    class RecordingIO : TestCaseIO
     {
         private readonly string inputFile;
 
@@ -288,38 +298,31 @@ namespace TestSuite
             this.inputFile = newInputFile;
         }
 
-        #region IZMachineIO Members
-
-        string IZMachineIO.ReadLine(string initial, int time, TimedInputCallback callback, byte[] terminatingKeys, out byte terminator)
+        public override string ReadLine(string initial, int time, TimedInputCallback callback, byte[] terminatingKeys, out byte terminator)
         {
             terminator = 13;
             return Console.ReadLine();
         }
 
-        void IZMachineIO.PutCommand(string command)
-        {
-            // nada
-        }
-
-        short IZMachineIO.ReadKey(int time, TimedInputCallback callback, CharTranslator translator)
+        public override short ReadKey(int time, TimedInputCallback callback, CharTranslator translator)
         {
             ConsoleKeyInfo info = Console.ReadKey(true);
             return translator(info.KeyChar);
         }
 
-        void IZMachineIO.PutChar(char ch)
+        public override void PutChar(char ch)
         {
             Console.Write(ch);
             outputBuffer.Append(ch);
         }
 
-        void IZMachineIO.PutString(string str)
+        public override void PutString(string str)
         {
             Console.Write(str);
             outputBuffer.Append(str);
         }
 
-        void IZMachineIO.PutTextRectangle(string[] lines)
+        public override void PutTextRectangle(string[] lines)
         {
             foreach (string str in lines)
             {
@@ -328,44 +331,7 @@ namespace TestSuite
             }
         }
 
-        bool IZMachineIO.Buffering
-        {
-            get { return false; }
-            set { /* nada */ }
-        }
-
-        bool IZMachineIO.Transcripting
-        {
-            get { return false; }
-            set { /* nada */ }
-        }
-
-        void IZMachineIO.PutTranscriptChar(char ch)
-        {
-            // nada
-        }
-
-        void IZMachineIO.PutTranscriptString(string str)
-        {
-            // nada
-        }
-
-        Stream IZMachineIO.OpenSaveFile(int size)
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenRestoreFile()
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenAuxiliaryFile(string name, int size, bool writing)
-        {
-            return null;
-        }
-
-        Stream IZMachineIO.OpenCommandFile(bool writing)
+        public override Stream OpenCommandFile(bool writing)
         {
             if (!writing)
                 return null;
@@ -373,171 +339,9 @@ namespace TestSuite
             return new FileStream(inputFile, FileMode.Create, FileAccess.Write);
         }
 
-        void IZMachineIO.SetTextStyle(TextStyle style)
-        {
-            // nada
-        }
-
-        void IZMachineIO.SplitWindow(short lines)
-        {
-            // nada
-        }
-
-        void IZMachineIO.SelectWindow(short num)
-        {
-            // nada
-        }
-
-        void IZMachineIO.EraseWindow(short num)
-        {
-            // nada
-        }
-
-        void IZMachineIO.EraseLine()
-        {
-            // nada
-        }
-
-        void IZMachineIO.MoveCursor(short x, short y)
-        {
-            // nada
-        }
-
-        void IZMachineIO.GetCursorPos(out short x, out short y)
-        {
-            x = 1;
-            y = 1;
-        }
-
-        void IZMachineIO.SetColors(short fg, short bg)
-        {
-            // nada
-        }
-
-        short IZMachineIO.SetFont(short num)
-        {
-            // not supported
-            return 0;
-        }
-
-        void IZMachineIO.PlaySoundSample(ushort number, SoundAction action, byte volume, byte repeats, SoundFinishedCallback callback)
-        {
-            // nada
-        }
-
-        void IZMachineIO.PlayBeep(bool highPitch)
+        public override void PlayBeep(bool highPitch)
         {
             Console.Beep(highPitch ? 1600 : 800, 200);
         }
-
-        bool IZMachineIO.ForceFixedPitch
-        {
-            get { return true; }
-            set { /* nada */ }
-        }
-
-        bool IZMachineIO.BoldAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.ItalicAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.FixedPitchAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.VariablePitchAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.ScrollFromBottom
-        {
-            get { return false; }
-            set { /* nada */ }
-        }
-
-        bool IZMachineIO.GraphicsFontAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.TimedInputAvailable
-        {
-            get { return false; }
-        }
-
-        bool IZMachineIO.SoundSamplesAvailable
-        {
-            get { return false; }
-        }
-
-        byte IZMachineIO.WidthChars
-        {
-            get { return 80; }
-        }
-
-        short IZMachineIO.WidthUnits
-        {
-            get { return 80; }
-        }
-
-        byte IZMachineIO.HeightChars
-        {
-            get { return 25; }
-        }
-
-        short IZMachineIO.HeightUnits
-        {
-            get { return 25; }
-        }
-
-        byte IZMachineIO.FontHeight
-        {
-            get { return 1; }
-        }
-
-        byte IZMachineIO.FontWidth
-        {
-            get { return 1; }
-        }
-
-        event EventHandler IZMachineIO.SizeChanged
-        {
-            add { /* nada */ }
-            remove { /* nada */ }
-        }
-
-        bool IZMachineIO.ColorsAvailable
-        {
-            get { return false; }
-        }
-
-        byte IZMachineIO.DefaultForeground
-        {
-            get { return 9; }
-        }
-
-        byte IZMachineIO.DefaultBackground
-        {
-            get { return 2; }
-        }
-
-        UnicodeCaps IZMachineIO.CheckUnicode(char ch)
-        {
-            return UnicodeCaps.CanInput | UnicodeCaps.CanPrint;
-        }
-
-        bool IZMachineIO.DrawCustomStatusLine(string location, short hoursOrScore, short minsOrTurns, bool useTime)
-        {
-            return false;
-        }
-
-        #endregion
     }
 }
